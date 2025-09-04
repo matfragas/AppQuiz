3e966d9bcfa9493b6bf133cb338763803913ad77
matfragas <franck.gasseau@gmail.com>
2025-09-04 14:26:32 +0200

    Delete src/pages/Home.jsx

--- src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="p-6 text-center">
-      <h1 className="text-2xl font-bold">🎯 Quiz App</h1>
-      <p>Bienvenue sur ton quiz administrable 🚀</p>
-    </div>
-  );
-}
